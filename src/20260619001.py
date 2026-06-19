6ddc4d7d448395551829ee992a4d82092936e1d1
Tintinopedia <ahaan@tintinopedia.com>
2026-06-19 12:18:52 +0530

    Add string functions homework exercise

---
+++ src/20260619001.py
@@ -0,0 +1,15 @@
+"""
+Homework reminder: Practice all the string functions
+(upper, lower, title, replace, count, len)
+as mentioned in class with any of your paragraph choices.
+"""
+
+paragraph = "Python is a fun language to learn. Python is also very powerful."
+
+print(paragraph.upper())
+print(paragraph.lower())
+print(paragraph.title())
+print(paragraph.replace("Python", "Coding"))
+print(paragraph.count("Python"))
+print(len(paragraph))
+
