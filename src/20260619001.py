0b523d90f8f706c3f33035a851897dde4064afd6
Tintinopedia <ahaan@tintinopedia.com>
2026-06-19 12:54:09 +0530

    Homework Commits

--- src/20260619001.py
+++ src/20260619001.py
@@ -1,7 +1,8 @@
 """
 Homework reminder: Practice all the string functions
 (upper, lower, title, replace, count, len)
-as mentioned in class with any of your paragraph choices.
+as mentioned in class using the below paragraph:
+"Python is a fun language to learn. Python is also very powerful."
 """
 
 paragraph = "Python is a fun language to learn. Python is also very powerful."
---
+++ src/20260619002.py
@@ -0,0 +1,9 @@
+"""
+Homework reminder: Practice all the string functions
+(upper, lower, title, replace, count, len)
+as mentioned in class using the below paragraph:
+
+"Ahaan is in 3rd Standard and loves to play Chess & Table Tennis.
+He also loves to Code in Python and play games on PS5."
+"""
+
